03f928274cb6b97b84d77984533e63833ff28250
Ruth Vazquez <rthvazquez@gmail.com>
2019-09-19 15:01:04 -0700

    changed the option layout of the survey

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+
+var friends = [
+	{
+		name: "Jack Kerouac",
+		photo: "https://www.theamericanconservative.com/wp-content/uploads/2018/02/Kerouac_by_Palumbo_2-e1518467210120.png",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Edith Piaf",
+		photo: "https://www.biography.com/.image/t_share/MTQzMjg0NDA3OTg4Mzk3NjM5/edith-piaf_gettyimages-104500550-1jpg.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Eartha Kitt",
+		photo: "http://info.umkc.edu/womenc/wp-content/uploads/2018/02/Eartha-Kitt.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Walt Whitman",
+		photo: "https://www.biography.com/.image/t_share/MTE5NTU2MzE2NDA0ODExMjc1/walt-whitman-9530126-1-402.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Claude Monet",
+		photo: "https://leslieparke.com/wp-content/uploads/2014/02/904941-claude-monet.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
